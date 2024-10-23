673dc961d18b053ad1939efb1387922c1239e099
Rayyan Hussain <37162004@edu.cbe.ab.ca>
2024-10-23 15:26:07 -0600

    completed gamestate test incorrectly. Redo by getting current game from active sessions object

--- server.js
+++ server.js
@@ -18,20 +18,22 @@ server.get("/newgame", (req, res)=> {
         gameOver: false
     }
     activeSessions[newID] = newGame
+    console.log(activeSessions)
     res.status(201)
     res.send({sessionID: newID})
 })
 
-// server.get("/gamestate", (req, res) => {
-//     res.send(200)
-//     let gameSate = {
-//         guesses:[],
-//         wrongLetters: [],
-//         closeLetters: [], //'a' is no longer close because it has been guessed in the correct spot
-//         rightLetters: [],
-//         remainingGuesses: 6,
-//     }
-// })
+server.get("/gamestate", (req, res) => {
+    let gameState = {
+        guesses:[],
+        wrongLetters: [],
+        closeLetters: [], //'a' is no longer close because it has been guessed in the correct spot
+        rightLetters: [],
+        remainingGuesses: 6,
+    }
+    res.status(200)
+    res.send({gameState})
+})
 //Do not remove this line. This allows the test suite to start
 //multiple instances of your server on different ports
 module.exports = server;
